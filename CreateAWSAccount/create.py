d3fbf0eddac53612da9f7e6da9b636f27a792f40
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-10-23 21:51:18 -0500

    new methods

--- CreateAWSAccount/create.py
+++ CreateAWSAccount/create.py
@@ -8,3 +8,20 @@ response = client.create_user(
     PermissionsBoundary='arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess' #ARN of policy
 )
 
+print(response)
+time.sleep(15)
+
+response = client.add_user_to_group(
+    GroupName='AdminGroup',
+    UserName='123xedward'
+)
+
+print(response)
+time.sleep(15)
+
+response = client.create_access_key(
+    UserName='123xedward' #return access key and secret access key
+)
+
+print(response)
+time.sleep(15)
